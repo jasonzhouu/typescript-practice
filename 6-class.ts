245955387cbc04da1e818ecb970a03641eb6a81f
Yusheng Zhou <jasonzhouu@gmail.com>
2019-03-31 16:50:42 +0800

    继承，多层继承，覆盖父类的方法

--- 6-class.ts
+++ 6-class.ts
@@ -15,3 +15,36 @@ class Person {
 let jason = new Person('Jason', 'Zhou');
 jason.getName();
 console.log(jason.firstName)
+
+
+// 继承
+class Man extends Person {
+    drive():void {
+        console.log('driving')
+    }
+}
+
+let jason2 = new Man('Jason', 'Zhou')
+jason2.drive()
+
+
+// 多重继承
+class ChineseMan extends Man {
+    region: 'China';
+    greet():void {
+        console.log('你好')
+    }
+}
+
+let jason3 = new ChineseMan('宇盛', '周');
+jason3.greet()
+
+// 重新父类的方法
+class ChineseDriver extends Man {
+    drive():void {
+        console.log('开车')
+    }
+}
+
+let jason4 = new ChineseDriver('宇盛', '周')
+jason4.drive()
